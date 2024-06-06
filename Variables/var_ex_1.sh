18f2a84f06649099503f3308320099f03e9b00d7
soymariavillaverde <soymariavillaverde@gmail.com>
2024-06-06 11:39:47 +0200

    Solved variables's first exercise

---
+++ Variables/var_ex_1.sh
@@ -0,0 +1,8 @@
+    echo "Este es el primer ejercicio de variables:"
+    
+    myName="María";
+    myAge=30;
+    myCity=Madrid;
+
+    echo "Mi nombre es $myName, tengo $myAge años y vivo en $myCity"
+ 
