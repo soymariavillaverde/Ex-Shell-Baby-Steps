c5656a6d7727d8d8527774d2babb59bb22896d17
soymariavillaverde <soymariavillaverde@gmail.com>
2024-06-06 11:46:09 +0200

    Solved variables's second exercise

---
+++ Variables/var_ex_2.sh
@@ -0,0 +1,8 @@
+    echo "Este es el segundo ejercicio de variables:"
+    
+    echo "¿Cuál es tu actividad favorita?"
+    read favoriteActivity
+    echo "¿Y cuál es tu comida favorita?"
+    read favoriteFood
+
+    echo "Mi actividad favorita es $favoriteActivity. Mi comida favorita es $favoriteFood"
